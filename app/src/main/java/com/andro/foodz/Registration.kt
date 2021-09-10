8af674bd78a4804197e699f99f10a0720606b3ed
aspire0071400 <prabhujivats@gmail.com>
2021-09-10 21:53:15 +0530

    fix previous error

--- app/src/main/java/com/andro/foodz/Registration.kt
+++ app/src/main/java/com/andro/foodz/Registration.kt
@@ -1,19 +1,20 @@
 package com.andro.foodz
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.andro.foodz.databinding.ActivityLoginFragmentBinding
+import com.andro.foodz.databinding.ActivityRegistrationBinding
 
-class Login : Fragment() {
-    lateinit var  login : ActivityLoginFragmentBinding
+class Registration :Fragment() {
+    lateinit var Registration: ActivityLoginFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?
     ): View {
-        login = ActivityLoginFragmentBinding.inflate(inflater)
-        return login.root
+        Registration = ActivityLoginFragmentBinding.inflate(layoutInflater)
+        return Registration.root
     }
-
 }
