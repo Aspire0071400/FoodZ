9a0b4b43ea9db5ce2d4dc7546f6937141b772f66
aspire0071400 <prabhujivats@gmail.com>
2021-09-19 22:52:35 +0530

    splash screen instantiated

--- app/src/main/java/com/andro/foodz/SplashScreen.kt
+++ app/src/main/java/com/andro/foodz/SplashScreen.kt
@@ -1,11 +1,21 @@
 package com.andro.foodz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.andro.foodz.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var splashscreen: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        splashscreen = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(splashscreen.root)
+
+        Thread {
+            Thread.sleep(1000)
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }.start()
     }
 }
