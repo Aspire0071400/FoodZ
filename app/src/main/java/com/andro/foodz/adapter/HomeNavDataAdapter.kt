b10102310aa134bc7dcceae6ac9fb80587c46d85
aspire0071400 <prabhujivats@gmail.com>
2021-09-30 21:42:14 +0530

    api on home frag made working but temp error in mainpage.kt file to be resolved later

--- app/src/main/java/com/andro/foodz/adapter/HomeNavDataAdapter.kt
+++ app/src/main/java/com/andro/foodz/adapter/HomeNavDataAdapter.kt
@@ -4,29 +4,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.work.Data
 import com.andro.foodz.databinding.FragmentHomeBinding
+import com.andro.foodz.databinding.HomeNavDataViewBinding
 import com.andro.foodz.model.HomeNavDataModel
 import com.bumptech.glide.Glide
 
 class HomeNavDataAdapter (private val data:ArrayList<HomeNavDataModel>): RecyclerView.Adapter<HomeNavDataAdapter.Data>() {
-
     class Data(v: View) : RecyclerView.ViewHolder(v) {
-        var HomeNavDataAdapter = FragmentHomeBinding.bind(v)
+        var HomeNavDataAdapter = HomeNavDataViewBinding.bind(v)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeNavDataAdapter.Data {
-        val adapter = FragmentHomeBinding.inflate(LayoutInflater.from(parent.context))
+        val adapter = HomeNavDataViewBinding.inflate(LayoutInflater.from(parent.context))
         return Data(adapter.root)
     }
 
-    override fun onBindViewHolder(holder: FragmentHomeBinding.Data, position: Int) {
-        holder.FragmentHomeBinding.HomeNavDataProductName.text = data[position].ProductName
-        holder.FragmentHomeBinding.HomeNavDataPrice.text = data[position].Price
-        holder.FragmentHomeBinding.HomeNavDataDescription.text = data[position].Description
-        holder.FragmentHomeBinding.HomeNavDataCategory.text = data[position].Category
+    override fun onBindViewHolder(holder: HomeNavDataAdapter.Data, position: Int) {
+        holder.HomeNavDataAdapter.ProductName.text=data[position].ProductName
+        holder.HomeNavDataAdapter.Price.text = data[position].Price
+        holder.HomeNavDataAdapter.Description.text = data[position].Description
+        holder.HomeNavDataAdapter.Category.text = data[position].Category
         Glide.with(holder.HomeNavDataAdapter.root).load(data[position].Image)
-            .into(holder.HomeNavDataAdapter.HomeNavDataImage)
+            .into(holder.HomeNavDataAdapter.image)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/andro/foodz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/andro/foodz/ui/home/HomeFragment.kt
@@ -1,25 +1,25 @@
 package com.andro.foodz.ui.home
 
-import android.app.DownloadManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.andro.foodz.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.andro.foodz.adapter.HomeNavDataAdapter
 import com.andro.foodz.databinding.FragmentHomeBinding
+import com.andro.foodz.model.HomeNavDataModel
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class HomeFragment : Fragment() {
-
     private lateinit var fragmenthome : FragmentHomeBinding
-    private var _binding: FragmentHomeBinding? = null
+    private var data = arrayListOf<HomeNavDataModel>()
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?
     ): View? {
@@ -30,11 +30,28 @@ class HomeFragment : Fragment() {
 
     private fun fetchData() {
         val request: RequestQueue= Volley.newRequestQueue(context)  //url
-        val stringRequest:StringRequest = StringRequest(
-            Request.Method.GET,"https://foodzproject.000webhostapp.com/fetch_data.php",{
-                val 
-            }
 
-        )
+        val stringRequest: StringRequest = StringRequest(
+            Request.Method.GET,"https://foodzproject.000webhostapp.com/fetch_data.php",{
+                val homenavdataArray:JSONArray=JSONArray(it)
+                for (i in 0 until homenavdataArray.length()){
+                    val homenavdata:JSONObject=homenavdataArray.get(i) as JSONObject
+                    val homenavmodeldata=HomeNavDataModel(homenavdata.getString("ProductName").toString(),
+                        (homenavdata.getString("Price").toString()),
+                        (homenavdata.getString("Description").toString()),
+                        (homenavdata.getString("Category").toString()),
+                        (homenavdata.getString("Image").toString()))
+                    data.add(homenavmodeldata)
+                }
+                fragmenthome.homeNavDataView.layoutManager=LinearLayoutManager(context)
+                fragmenthome.homeNavDataView.adapter=HomeNavDataAdapter(data)
+
+            },{
+                //log.d("Error",it.toString())
+                if (it.networkResponse.statusCode == 404){
+                        fetchData()
+                }
+            })
+                request.add(stringRequest)
     }
 }
