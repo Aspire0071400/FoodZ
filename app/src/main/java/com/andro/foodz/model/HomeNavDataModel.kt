9a594c3574c00339223b6c722da54cb57ed185dd
ManasSardana <manassardana@gmail.com>
2021-10-01 16:27:57 +0530

    rectified

---
+++ app/src/main/java/com/andro/foodz/model/HomeNavDataModel.kt
@@ -0,0 +1,8 @@
+package com.andro.foodz.model
+
+class HomeNavDataModel(
+    val ProductName: String,
+    val Price: String,
+    val Description: String,
+    val Category: String,
+    val Image: String) { }
