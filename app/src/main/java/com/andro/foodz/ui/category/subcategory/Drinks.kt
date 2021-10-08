578011961ab8135a646439fef87a7f030bb73131
aspire0071400 <prabhujivats@gmail.com>
2021-10-08 13:03:11 +0530

    dark theme deleted sub category added

---
+++ app/src/main/java/com/andro/foodz/ui/category/subcategory/Drinks.kt
@@ -0,0 +1,9 @@
+package com.andro.foodz.ui.category.subcategory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class Drinks : Fragment() {
+
+}
