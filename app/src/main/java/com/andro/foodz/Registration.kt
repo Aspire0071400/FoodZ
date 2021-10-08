51707afde85cc4a4ca8575913cde50b0b1ddaba2
Varun9650 <varunkaushik993@gmail.com>
2021-10-08 12:46:38 +0530

    Category Menu Click is working now

--- app/src/main/java/com/andro/foodz/Registration.kt
+++ app/src/main/java/com/andro/foodz/Registration.kt
@@ -1,11 +1,12 @@
 package com.andro.foodz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+
+
 import com.andro.foodz.databinding.ActivityRegistrationBinding
 
 class Registration :Fragment() {
