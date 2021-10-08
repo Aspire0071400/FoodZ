5915a38282396a4272b5891d81c348d7660ffbc7
aspire0071400 <prabhujivats@gmail.com>
2021-10-08 12:19:22 +0530

    category frag and sub frag created with data model

--- app/src/main/java/com/andro/foodz/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/andro/foodz/ui/category/CategoryFragment.kt
@@ -5,49 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.andro.foodz.MainPage
 import com.andro.foodz.databinding.FragmentCategoryBinding
-import com.andro.foodz.ui.category.catfrag.Drinks
-import com.andro.foodz.ui.category.catfrag.FastFood
-import com.andro.foodz.ui.category.catfrag.MainCourse
-import com.andro.foodz.ui.category.catfrag.Sweets
 
 class CategoryFragment : Fragment() {
     lateinit var fragmentcategory : FragmentCategoryBinding
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         fragmentcategory = FragmentCategoryBinding.inflate(inflater)
         fragmentcategory.mainCourseImg.setOnClickListener {
-            (activity as MainPage).change(MainCourse())
+            //(activity as MainPage).change(MainCourse())
         }
 
         fragmentcategory.drinksImg.setOnClickListener {
-            (activity as MainPage).change(Drinks())
+            //(activity as MainPage).change(Drinks())
         }
 
         fragmentcategory.fastFoodImg.setOnClickListener {
-            (activity as MainPage).change(FastFood())
+           // (activity as MainPage).change(FastFood())
         }
 
         fragmentcategory.sweetsImg.setOnClickListener {
-            (activity as MainPage).change(Sweets())
+           // (activity as MainPage).change(Sweets())
         }
 
         fragmentcategory.mainCourse.setOnClickListener {
-            (activity as MainPage).change(MainCourse())
+            //(activity as MainPage).change(MainCourse())
         }
 
         fragmentcategory.drinks.setOnClickListener {
-            (activity as MainPage).change(Drinks())
+            //(activity as MainPage).change(Drinks())
         }
 
         fragmentcategory.fastFood.setOnClickListener {
-            (activity as MainPage).change(FastFood())
+          //  (activity as MainPage).change(FastFood())
         }
 
         fragmentcategory.sweets.setOnClickListener {
-            (activity as MainPage).change(Sweets())
+           // (activity as MainPage).change(Sweets())
         }
 
 
