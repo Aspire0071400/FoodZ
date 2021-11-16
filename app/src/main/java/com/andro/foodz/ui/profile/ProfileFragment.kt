8b1cdf05b51c2755a4c4be12b50f8578a192abf8
Varun Kaushik <varunkaushik993@gmail.com>
2021-11-16 20:59:14 +0530

    Profile fetch data

--- app/src/main/java/com/andro/foodz/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/andro/foodz/ui/profile/ProfileFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.andro.foodz.databinding.FragmentProfileBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ProfileFragment : Fragment() {
@@ -18,19 +20,26 @@ class ProfileFragment : Fragment() {
     var number: String? =null
     var address: String? =null
 
+    lateinit var auth: FirebaseAuth
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?
     ): View? {
         fragmentprofile = FragmentProfileBinding.inflate(inflater)
-
+        auth = FirebaseAuth.getInstance()
 
 
         fragmentprofile.profileSave.setOnClickListener {
-            name   = fragmentprofile.profileName.text.toString()
-            email  =fragmentprofile.profileEmail.text.toString()
-            number =fragmentprofile.profileNumber.text.toString()
-            address= fragmentprofile.profileAddress.text.toString()
+            val user = auth.currentUser
+            name = user?.displayName.toString()
+            email = user?.email.toString()
+            Toast.makeText(requireContext(), "$name, $email", Toast.LENGTH_SHORT).show()
+
+            fragmentprofile.profileName.setText(name)
+            fragmentprofile.profileEmail.setText(email)
+            //number =fragmentprofile.profileNumber.text.toString()
+            //address= fragmentprofile.profileAddress.text.toString()
             fragmentprofile.profileEdit.isVisible=true
             fragmentprofile.profileSave.isVisible=false
             Snackbar.make(fragmentprofile.root,"Your Profile has been saved",Snackbar.LENGTH_LONG).show()
